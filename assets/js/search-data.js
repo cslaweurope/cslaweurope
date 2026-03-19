764632a271290daf23df3f9b362b1263afa2617b
Aurelia-ATL <71078821+Aurelia-ATL@users.noreply.github.com>
2026-03-19 13:33:08 +0000

    Deploying to gh-pages from @ cslaweurope/cslaweurope.github.io@66dd5c4d89e7075858353392dea7b410ea77577f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-activities",
           title: "activities",
-          description: "Activities organized by the CS&amp;Law Europe Network.",
+          description: "Activities organized by the CS&amp;Law Europe Network. These monthly activities rotate and take place every last Friday of the month from 12.30-1.30 CET -- sign up with the Google sheet to get a calendar invite.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/activities/";
